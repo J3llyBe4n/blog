cfc2938996b2fd09ff108422fc61f1b23c6c42e2
J3llyBe4n <zza0423@gmail.com>
2022-08-17 23:50:29 +0900

    5th commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
       title: `주니어 개발자의 개발 블로그`,
       description: `주니어 개발자로서의 저를 표현한 블로그입니다.`,
       author: `Hyun`,
-      siteUrl: 'https://J3llyBe4n.github.io/',
+      siteUrl: 'https://j3llyBe4n.github.io/',
   },
 
   plugins: [
